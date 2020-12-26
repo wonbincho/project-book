868d757fb33f52cafeb5048f0a3d30aea1dde11e
wonbincho <wjdqkseo@gmail.com>
2020-12-26 12:51:30 +0900

    코드정리

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ class Todo_list {
   add(text) {
     if (text != "") {
       const addlist = {
-        Id: list.length,
-        Text: text,
+        id: list.length,
+        text,
       }
       list.push(addlist);
       text = "";
@@ -22,11 +22,11 @@ class Todo_list {
 
     //forEach() 메서드는 주어진 함수를 배열 요소 각각에 대해 실행합니다.
     list.forEach((list) => {
-      const { Id, Text } = list;
+      const { id, text } = list;
       const newListItem = document.createElement('li'); // 새로운 li 요소를 만듬
       newListItem.classList.add('listItem'); // listItem 클래스를 더해줌
-      newListItem.setAttribute("id", Id);
-      newListItem.innerText = Text; //사용자 입력 문자를 li사이에 넣음
+      newListItem.setAttribute("id", id);
+      newListItem.innerText = text; //사용자 입력 문자를 li사이에 넣음
       this.ulElement.appendChild(newListItem); // 리스트에 새로 만든 li를 추가 -> 요소추가하기
 
       newListItem.addEventListener("click", () => {
@@ -37,7 +37,7 @@ class Todo_list {
   }
   header(select) {
     const headerEvent = document.querySelector("#header");
-    headerEvent.innerHTML = list[select].Text;
+    headerEvent.innerHTML = list[select].text;
 
   }
 }
@@ -46,7 +46,7 @@ const listSection = document.querySelector("#showlist")
 
 let Todolist = new Todo_list(listSection);
 
-addlist.addEventListener('click', () => {
+additem.addEventListener('click', () => {
   let text = prompt("목록");
   Todolist.add(text);
 });
