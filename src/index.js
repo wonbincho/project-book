dd1fba3ca3d1a302cbd27085a19f0e455610398a
wonbincho <wjdqkseo@gmail.com>
2021-01-26 20:33:36 +0900

    이슈해결

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export const Calculator = () => {
     const calculator = [];
-    const screen = document.getElementsByClassName('input')[0];
+    const screen = document.querySelector('.input');
 
     const number = document.getElementsByClassName('num');
     const cal = document.getElementsByClassName('cal');
@@ -12,12 +12,12 @@ export const Calculator = () => {
     let count = 0;
     let len = 0;
 
-    if (screen.value == '0') {
+    if (screen.value === '0') {
         screen.value = "";
     }
 
     result_btn.addEventListener('click', () => {
-        if (count == 1) {
+        if (count === 1) {
             calculator[len] = screen.value;
             calculate(calculator.length - 1);
             count = 2;
@@ -26,22 +26,22 @@ export const Calculator = () => {
 
     for (let i = 0; i < number.length; i++) {
         number[i].addEventListener('click', () => {
-            let value = number[i].value;
-            if (count == 0) {
+            const value = number[i].value;
+            if (count === 0) {
                 screen.value = '';
                 len = calculator.length;
             }
-            screen.value = screen.value + value;
-            if (len == 0) result = screen.value;
+            screen.value += value;
+            if (len === 0) result = screen.value;
             count = 1;
         })
     }
     for (let i = 0; i < cal.length; i++) {
         cal[i].addEventListener('click', () => {
             calculator[len] = screen.value;
-            if (count == 1 || count == 2) {
+            if (count === 1 || count === 2) {
                 calculator[calculator.length] = cal[i].value
-                if (calculator.length > 2 && count != 2) calculate(calculator.length - 2);
+                if (calculator.length > 2 && count !== 2) calculate(calculator.length - 2);
             }
             count = 0;
         })
@@ -58,23 +58,23 @@ export const Calculator = () => {
 
         console.log(calculator);
         console.log(len, len - 1, len - 2, len - 3);
-        if (calculator[len - 1] == '+') {
+        if (calculator[len - 1] === '+') {
             result = Number(result) + Number(calculator[len]);
             screen.value = result;
         }
-        else if (calculator[len - 1] == '-') {
+        else if (calculator[len - 1] === '-') {
             result = Number(result) - Number(calculator[len]);
             screen.value = result;
         }
-        else if (calculator[len - 1] == '*') {
+        else if (calculator[len - 1] === '*') {
             result = Number(result) * Number(calculator[len]);
             screen.value = result;
         }
-        else if (calculator[len - 1] == '/') {
+        else if (calculator[len - 1] === '/') {
             result = Number(result) / Number(calculator[len]);
             screen.value = result;
         }
-        else if (calculator[len - 1] == '%') {
+        else if (calculator[len - 1] === '%') {
             result = Number(result) % Number(calculator[len]);
             screen.value = result;
         }
